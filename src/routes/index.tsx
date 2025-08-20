a0483377a82ae26509353ca56697f3db2a6edc1f
lukkaz2709 <lmiguez2709@gmail.com>
2025-08-20 14:33:10 -0300

    drawer.routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import {StackRoutes} from "./stack.routes";
 import { BottomTabsRoutes } from "./botom-tabs.routes";
+import { DrawerRoutes } from "./drawer.routes";
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <BottomTabsRoutes />
+      <DrawerRoutes />
     </NavigationContainer>
   );
 }
